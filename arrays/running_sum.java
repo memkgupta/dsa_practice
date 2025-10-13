ae44c27ca8642a66dfc3a9d98aef2d968cd4676e
= <=>
2025-10-14 00:21:47 +0530

    running_sum

---
+++ arrays/running_sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        // it is simple prefix sum 
+        int[] sums = new int[nums.length];
+        sums[0] = nums[0];
+        for(int i = 1;i<nums.length;i++)
+        {
+                sums[i] = sums[i-1]+nums[i];
+        }
+        return sums;
+    }
+}
